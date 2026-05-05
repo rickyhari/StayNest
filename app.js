2cb87547b3faa7f9966a660da4e49f96638d7940
rickyhari <rickyhari2006@gmail.com>
2026-05-06 04:12:00 +0530

    final: ready for deployment

--- app.js
+++ app.js
@@ -9,21 +9,18 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
+const MongoStore = require("connect-mongo").default;
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError.js");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
-// const wrapAsync = require("./utils/wrapAsync.js");
-// const { listingSchema, reviewSchema } = require("./schema.js");
-// const Listing = require("./models/listing.js");
-// const Review = require("./models/review.js");
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/staynest";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -34,7 +31,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -44,8 +41,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", (err) => {
+    console.log("error on MONGO SESSION STORE", err);
+});
+
 const sessionOptions = {
-  secret: "mysupersecretcode",
+  store,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,7 @@ module.exports.signup = async (req, res) => {
             if(err){
                 return next(err);
             }
-            req.flash("success", "Welcome to WanderLust!");
+            req.flash("success", "Welcome to StayNest!");
             res.redirect("/listings");
         });
     }
@@ -30,7 +30,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = async (req, res) => {
-    req.flash("success", "Welcome back to WanderLust!");
+    req.flash("success", "Welcome back to StayNest!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 };
