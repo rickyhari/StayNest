c0c7396958e354d4455a8b7eaf5cdf957595d03a
rickyhari <rickyhari2006@gmail.com>
2026-05-06 12:22:22 +0530

    refactor: remove obsolete backfill script after integrating geometry into seed data

--- init/index.js
+++ init/index.js
@@ -1,29 +1,60 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const axios = require("axios");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/staynest";
-
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const dbUrl = process.env.ATLASDB_URL;
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
+  console.log("connected to DB");
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({
-    ...obj,
-    owner: "69f0bad29fca99a2a4d9ba20",
-  }));
-  await Listing.insertMany(initData.data);
-  console.log("data was initialised");
+  console.log("Old data deleted");
+
+  for (let obj of initData.data) {
+    try {
+      const url = `https://api.opencagedata.com/geocode/v1/json?q=${obj.location}&key=${process.env.OPENCAGE_API_KEY}`;
+
+      const response = await axios.get(url);
+
+      let geometry = {
+        type: "Point",
+        coordinates: [0, 0], // fallback
+      };
+
+      if (response.data.results.length > 0) {
+        const { lat, lng } = response.data.results[0].geometry;
+
+        geometry = {
+          type: "Point",
+          coordinates: [lng, lat],
+        };
+      }
+
+      const newListing = new Listing({
+        ...obj,
+        owner: "69fa6f5cc240f26fbb6dbb91",
+        geometry,
+      });
+
+      await newListing.save();
+      console.log(`Added: ${obj.title}`);
+    } catch (err) {
+      console.log(`Error in ${obj.title}:`, err.message);
+    }
+  }
+
+  console.log("✅ Data was initialized with geometry");
+  mongoose.connection.close();
 };
 
-initDB();
+main()
+  .then(() => initDB())
+  .catch((err) => console.log(err));
